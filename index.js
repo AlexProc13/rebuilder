7e60376e1ad027e8ad0e5e806c45d7a7404c2b5f
alexproc13 <alexproc1313@gmail.com>
2021-02-04 10:15:01 +0200

    test

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ const PORT = 7777;
 app.use(express.json());
 
 app.get('/rebuild', (request, response) => {
-  let platform = request.query.platform;
-  //let dir = `/var/www/${platform}`;
-  let command = `cd ${dir} && npm run build`;
-
-  if (fs.existsSync(dir)) {
-    exec(command, function callback(error, stdout, stderr){
-      // result
-      console.log(error);
-    });
-  }
-  response.send('Ok');
+    let platform = request.query.platform;
+    let dir = `/var/www/${platform}`;
+    if (platform != 'undefined' && fs.existsSync(dir)) {
+        let command = `cd ${dir} && npm run build`;
+        exec(command, function callback(error, stdout, stderr) {
+            // result
+            console.log(error);
+        });
+    }
+    response.send('Ok');
 });
 
 app.listen(PORT, () => console.log(`Server currently running on port ${PORT}`));
