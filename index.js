7c6c6b346dc3bf6f1327ac84dd329c87e2e35a61
alexproc13 <alexproc1313@gmail.com>
2021-02-04 10:03:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exec = require('child_process').exec;
+const fs = require("fs");
+
+const app = express();
+const PORT = 7777;
+
+app.use(express.json());
+
+app.get('/rebuild', (request, response) => {
+  let platform = request.query.platform;
+  //let dir = `/var/www/${platform}`;
+  let dir = `/home/user/Desktop/files/projects/front.nuxbet`;
+  let command = `cd ${dir} && npm run build`;
+
+  if (fs.existsSync(dir)) {
+    exec(command, function callback(error, stdout, stderr){
+      // result
+      console.log(error);
+    });
+  }
+  response.send('Ok');
+});
+
+app.listen(PORT, () => console.log(`Server currently running on port ${PORT}`));
